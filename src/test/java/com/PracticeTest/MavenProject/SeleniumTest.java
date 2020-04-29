a4ed2fed634afba162dc5c2a097f06a8fe6d9c26
Diganta Bhaduri <diganta.bhaduri1493@gmail.com>
2020-04-29 10:45:03 +0530

    Initial Commit

---
+++ src/test/java/com/PracticeTest/MavenProject/SeleniumTest.java
@@ -0,0 +1,13 @@
+package com.PracticeTest.MavenProject;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest 
+{
+	@Test
+	public void Dummy1()
+	{
+		System.out.println("This is Dummy test 1");
+	}
+
+}
